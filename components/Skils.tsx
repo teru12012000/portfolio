4861857a9422a67ce523d2cc3d22a422efe31651
teru12012000 <sion0012@outlook.jp>
2022-10-13 14:49:46 +0900

    test

--- components/Skils.tsx
+++ components/Skils.tsx
@@ -15,18 +15,6 @@ type lang={
   start:string;
   level:string;
 }
-Modal.setAppElement('#__next');
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-    
-  },
-};
 const Skils: NextPage = () => {
   const [modalIsOpen,setModalIsOpen]=useState(false);
   const Skil:lang[]=[
