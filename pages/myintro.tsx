57bf85cbe3753aad0a1cd9b491531b6d4021e4a3
teru12012000 <sion0012@outlook.jp>
2023-01-31 21:06:01 +0900

    change style

--- pages/myintro.tsx
+++ pages/myintro.tsx
@@ -15,22 +15,36 @@ const profile: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header open={"flex"}/>
-      
-      <div className={intro.content} id="intro">
-        <h2 className={intro.h2}>自己紹介</h2>
-        <div className={intro.passage}>
+      <h2 
+        className={intro.h2}
+        style={{
+          marginTop:"200px",
+        }}
+      >
+        自己紹介
+      </h2>
+      <div 
+        className="container w-80 " 
+        id="intro"
+        style={{
+            backgroundColor:"silver"
+          }}
+      >
+        <div>
         {my_intro.map((item,index)=>(
-          <Card sx={{ maxWidth: 345 }} key={index} className="mt-5">
-            <Typography gutterBottom variant="h4" component="div">
-              {item.question}
-            </Typography>
-            <Typography gutterBottom variant="h5" component="div">
-              {item.answer}
-            </Typography>
-            <Typography gutterBottom component="div">
-              {item.detail}
-            </Typography>
-          </Card> 
+          <div key={index} className="d-inline-block m-5">
+            <Card sx={{ width: 200,height:350 }}>
+              <Typography gutterBottom variant="h4" component="div">
+                {item.question}
+              </Typography>
+              <Typography gutterBottom variant="h5" component="div">
+                {item.answer}
+              </Typography>
+              <Typography gutterBottom component="div">
+                {item.detail}
+              </Typography>
+            </Card> 
+          </div>
         ))}
         </div>
       </div>
