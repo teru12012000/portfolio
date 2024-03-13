8c0efad9c20d51d57b6308785d1fdaf8f5d072ca
teru12012000 <sion2769@gmail.com>
2024-03-14 00:06:22 +0900

    fix: delete card animation

--- pages/myintro.tsx
+++ pages/myintro.tsx
@@ -1,20 +1,20 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Box from '@mui/material/Box';
-import Header from '../components/Header'
-import { my_intro, my_profile } from '../data/profiledata'
-import { Card, Modal, Typography } from '@mui/material'
-import { AnimatePresence, motion } from 'framer-motion'
-import { useState } from 'react';
-import Menucard from '../components/Menucard'
-import Animate from '../components/Animate';
+import type { NextPage } from "next";
+import Head from "next/head";
+import Box from "@mui/material/Box";
+import Header from "../components/Header";
+import { my_intro, my_profile } from "../data/profiledata";
+import { Card, Modal, Typography } from "@mui/material";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
+import Menucard from "../components/Menucard";
+import Animate from "../components/Animate";
 const Profile: NextPage = () => {
-  const [selectId,setSelectId]=useState<string|null>(null);
+  const [selectId, setSelectId] = useState<string | null>(null);
   const [editModalIsOpen, setEditModalIsOpen] = useState<boolean>(false);
-  const openmodal=(id:string)=>{
+  const openmodal = (id: string) => {
     setSelectId(id);
     setEditModalIsOpen(true);
-  }
+  };
 
   return (
     <div>
@@ -23,24 +23,23 @@ const Profile: NextPage = () => {
         <meta name="description" content="プロフィールです" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header open={"flex"}/>
-      <Menucard title='自己紹介' marginSize={true}>
-        {my_intro.map((item:my_profile,index:number)=>(
-          <motion.div 
-            key={index} 
+      <Header open={"flex"} />
+      <Menucard title="自己紹介" marginSize={true}>
+        {my_intro.map((item: my_profile, index: number) => (
+          <div
+            key={index}
             className="d-inline-block m-4"
-            layoutId={String(index)}
-            onClick={()=>openmodal(String(index))}
+            onClick={() => openmodal(String(index))}
           >
-            <Card sx={{width:200}} className="text-start">
+            <Card sx={{ width: 200 }} className="text-start">
               <Typography gutterBottom variant="h4" component="div">
                 {item.question}
               </Typography>
               <Typography gutterBottom variant="h5" component="div">
                 {item.answer}
               </Typography>
-            </Card> 
-          </motion.div>
+            </Card>
+          </div>
         ))}
         <Animate
           selectId={selectId}
@@ -60,7 +59,7 @@ const Profile: NextPage = () => {
         </Animate>
       </Menucard>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
